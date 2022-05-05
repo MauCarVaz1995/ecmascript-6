d9ae87278bfdf279d859192cf402ed8a599f1ffa
Mauricio Carrillo <mcarrillov183@alumno.uaemex.mx>
2022-05-05 11:25:20 -0500

    Let, const, multilínea, spread operator y desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,6 @@
+//VALORES PREDEFINIDOS EN LAS FUNCIONES
+
+//Antes
 function newFuntion(name, age, country){
    var name = name || "Mau";
    var age = age || 26 ;
@@ -13,11 +16,68 @@ function newFunction2(name="Mau", age=26, country ="Mex"){
 newFunction2();
 newFunction2("Kevin", 30, "Mex")
 
-//Template literals
+//CONCATENACIÓN
+
+//antes
 let hello = "hello";
 let world = "world";
 let epicPhrase = hello + " "+ world;
 console.log(epicPhrase);
 
-let epicPhrase2 = `${hello} ${world}`; //esta la podemos usar con template literals en ecmascript 6
+//template literals en ecmascript 6
+let epicPhrase2 = `${hello} ${world}`;  
 console.log(epicPhrase2);
+
+//MULTILINEA
+
+//antes
+let lorem = "Quiero escribir una frase épica \n" 
++ "otra frase épica que necesitamos."
+
+//es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase épica.
+`
+console.log(lorem);
+console.log(lorem2);
+
+//DESESTRUCTURACIÓN DE ELEMENTOS
+
+//Antes
+let person = {
+    "nombre": "Mauricio",
+    "age": 26,
+    "country": "MX"
+}
+
+console.log(person.nombre, person.age);
+
+//es6
+let {nombre, age,} = person;
+console.log( nombre, age);
+
+//UNIÓN DE ELEMENTOS
+
+let team1 =["Mauricio", "Kevin", "Ricardo"];
+let team2 =["Daniela", "Paola", "Yolanda"];
+
+let education = ["David", ...team1, ...team2]; //es6
+
+console.log(education);
+
+
+//DIFERENCIA ENTRE VAR Y LET
+{
+    var globalVar = "Global Var";
+}
+{
+    let globalLet = "Global Let";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+//CONSTANTE QUE NO CAMBIA
+const a = "b";
+a = "c";
+
