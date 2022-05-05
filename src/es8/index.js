b215be9f1576d6ce4c45f6948279615a0be8e32e
Mauricio Carrillo <mcarrillov183@alumno.uaemex.mx>
2022-05-05 14:56:12 -0500

    es8

---
+++ src/es8/index.js
@@ -0,0 +1,33 @@
+//Object Entries
+const data = {
+    frontend: "Mauricio",
+    backend: "Alberto",
+    design: "Kevin"
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+//Object Values
+
+const data1 = {
+    frontend: "Mauricio",
+    backend: "Alberto",
+    design: "Kevin"
+}
+const values = Object.values(data1);
+console.log(values);
+console.log(values.length);
+
+//Padding
+const string = hello;
+console.log(string.padStart(7, "hI"));
+console.log(padEnd(12, " ----"));
+console.log("food".padEnd(12, "  ----"));
+
+const obj ={
+    frontend: "Mauricio",
+    backend: "Alberto",
+    design: "Kevin", //la coma para no cometer errore.
+}
