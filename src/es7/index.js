32961f6e48a9433b3881439dcc4f1e2e40939304
Mauricio Carrillo <mcarrillov183@alumno.uaemex.mx>
2022-05-05 14:13:27 -0500

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+//Método includes
+let numbers = [1,2,3,4,5,8];
+
+if (numbers.includes(7)){
+    console.log("Está el 7")
+}else{
+    console.log("no se encuentra")
+}
+
+//Operación de potencia
+
+let base =4;
+let exponent = 3;
+let result = base**exponent;
+
+console.log(result);
